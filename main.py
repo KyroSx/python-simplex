dc10636347d06fe425d7a718327702e9874f5e5f
Oxyps <abel_castro_junior@hotmail.com>
2020-07-23 22:53:19 -0300

    added recursion until optimal solution

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ def get_pivot_row_index(matrix, col):
 		index with minor positive number
 	'''
 
-	COL_LEN = matrix[0].__len__()
-	ROW_LEN = matrix.__len__()
-	
 	positive_rows = []
 	# starts at inequations row
 	i = 1
@@ -41,7 +38,6 @@ def get_new_pivot_row(pivot_row, pivot_number):
 
 def get_new_row(new_pivot_row, inversed_var, row):
 	multiplication = list(map(lambda number: number * inversed_var, new_pivot_row))
-	
 	new_row = []
 	for m, n in zip(multiplication, row):
 		new_row.append(m + n)
@@ -53,22 +49,63 @@ def get_new_matrix(matrix, pivot_row_index, new_pivot_row, pivot_col_index):
 		Return new matrix, calculate new rows
 	'''
 
-	COL_LEN = matrix[0].__len__()
-	ROW_LEN = matrix.__len__()
-
 	# just need to have already declared new matrix's indexes
 	new_matrix = []
 	for i in matrix:
 		new_matrix.append([])
 
 	i = 0
-	while i < ROW_LEN and i != pivot_row_index:
+	while i < ROW_LEN:
+		if i == pivot_row_index:
+			i += 1
+			continue
+
 		new_row = get_new_row(new_pivot_row, matrix[i][pivot_col_index] * (-1), matrix[i])
 		new_matrix[i] = new_row
 		i += 1
 
 	new_matrix[pivot_row_index] = new_pivot_row
 	return new_matrix
+
+def get_solution(matrix):
+	not_basic_vars = []
+	basic_vars = []
+
+	# starting at inequations col
+	j = 1
+	# while not in b col
+	while j < COL_LEN - 1:
+		# starting at inequations row
+		i = 1
+		while i < ROW_LEN:
+			if matrix[i][j] != 0 and matrix[i][j] != 1:
+				not_basic_vars.append(j)
+				break
+			
+			i += 1
+		
+		# if arrived at final row
+		if i == ROW_LEN:
+			basic_vars.append(j)
+
+		j += 1
+
+	return {
+		'variáveis básicas': basic_vars,
+		'variáveis não básicas': not_basic_vars,
+		'valor de z': matrix[0][COL_LEN - 1]
+	}
+
+def is_optimal(z_eq):
+	# start at vars
+	i = 1
+	# while not in b col
+	while i < COL_LEN - 1:
+		if z_eq[i] < 0:
+			return False
+		i += 1
+
+	return True
 
 def simplex(matrix):
 	pivot_col_index = get_pivot_col_index(matrix)
@@ -79,12 +116,28 @@ def simplex(matrix):
 	new_pivot_row = get_new_pivot_row(pivot_row, pivot_number)
 
 	new_matrix = get_new_matrix(matrix, pivot_row_index, new_pivot_row, pivot_col_index)
-	print(new_matrix)
+	solution = get_solution(new_matrix)
+	
+	if is_optimal(new_matrix[0]):
+		return solution
+	
+	return simplex(new_matrix)
+
+# table = [
+# 	[1, -10, -12, 0, 0, 0],
+# 	[0, 1, 1, 1, 0, 100],
+# 	[0, 1, 3, 0, 1, 270]
+# ]
 
 table = [
-	[1, -10, -12, 0, 0, 0],
-	[0, 1, 1, 1, 0, 100],
-	[0, 1, 3, 0, 1, 270]
+	[1, -3, -5, 0, 0, 0, 0],
+	[0, 2, 4, 1, 0, 0, 10],
+	[0, 6, 1, 0, 1, 0, 20],
+	[0, 1, -1, 0, 0, 1, 30]
 ]
 
-simplex(table)
+COL_LEN = table[0].__len__()
+ROW_LEN = table.__len__()
+
+solution = simplex(table)
+print(solution)
